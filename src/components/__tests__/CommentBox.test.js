fc75f5909e5e9e1518f0320ca0dc9cf6dd982b2d
Eden Szeto <edenszeto@hk01.com>
2018-06-30 19:10:40 +0800

    test: unmount component for safety reason

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -7,6 +7,11 @@ let wrapped;
 beforeEach(() => {
   wrapped = mount(<CommentBox />);
 });
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
 it('has a textarea and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
