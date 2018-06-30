edadb80ec50d700ffab2aaa943f62f78f9ef059e
Eden Szeto <edenszeto@hk01.com>
2018-06-30 19:08:45 +0800

    test: testing comment box that contains textarea and a button

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from '../CommentBox';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+it('has a textarea and a button', () => {
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
+});
