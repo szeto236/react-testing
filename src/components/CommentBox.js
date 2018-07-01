df7c2e5a71b1d27c748274994a6933fe94aebc77
Eden Szeto <edenszeto@hk01.com>
2018-07-02 01:51:17 +0800

    chore: add classname to fetch comments button for easier testing

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -28,7 +28,9 @@ class CommentBox extends Component {
             <button>Submit Comment</button>
           </div>
         </form>
-        <button onClick={this.props.fetchComments}>Fetch comments</button>
+        <button className="fetch-comments" onClick={this.props.fetchComments}>
+          Fetch comments
+        </button>
       </Fragment>
     );
   }
