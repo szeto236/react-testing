68d7d3338ba2dba850412dc2612e976fab5d6d2b
Eden Szeto <edenszeto@hk01.com>
2018-07-01 21:03:56 +0800

    refactor: refactor commentList to render the list of comments

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-  return <div>Comment List</div>;
-};
+function mapStateToProps(state) {
+  return {
+    comments: state.comments
+  };
+}
+
+class CommentList extends Component {
+  renderComments = () => {
+    return this.props.comments.map(comment => {
+      return <li key={comment}>{comment}</li>;
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h4>Comment List</h4>
+        <ul>{this.renderComments()}</ul>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(CommentList);
